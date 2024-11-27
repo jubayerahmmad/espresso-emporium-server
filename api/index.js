a27f373dfbb7195707767394c3f6e3547192ba08
jubayerahmmad <zubayersami510@gmail.com>
2024-11-27 11:59:01 +0600

    Trigger re-deployment

--- api/index.js
+++ api/index.js
@@ -6,11 +6,12 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const MONGO_URI = process.env.MONGO_URI;
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y41ia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y41ia.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y41ia.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(MONGO_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
